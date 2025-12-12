b420801236e51da9bb65ac9100446deafc66e067
PushCraft <consultant.jaskaran@gmail.com>
2025-12-12 13:08:39 -0500

    In

---
+++ gitpush.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo " Git Status: "
+git status
+
+git add *
+
+echo " Commiting changes : "
+git commit -m "Committing changes from user: $(whoami), time: $(date), machine: $(hostname)"
+
+
+git push origin main
+
+echo "Pushed ... "
